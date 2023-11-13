429127cf5006a87f8553c85488f648427636e913
Cristian06Meneses <cristianmeneses979@gmail.com>
2023-11-12 22:35:42 -0500

    Añadidos los productos

--- paginaweb/src/pagina/Encabezado.js
+++ paginaweb/src/pagina/Encabezado.js
@@ -1,14 +1,20 @@
 import React from "react";
-import Logo from '../imagenes/logoLicorera.png'
-import Icono from '../imagenes/iconoPersona.jpg'
-import '../HojasDeEstilo/Encabezado.css'
+import Logo from '../imagenes/logoLicorera.png';
+import Icono from '../imagenes/iconoPersona.jpg';
+import '../HojasDeEstilo/Encabezado.css';
+import {NavLink} from "react-router-dom";
+
 const Encabezado = () => {
     return(
         <div className="contenedorEncabezado">
-        <img className='logo'src={Logo} alt="imagen del logo"/>
+        <NavLink to='/productos'>
+            <img className='logo'src={Logo} alt="imagen del logo"/>
+        </NavLink>
         <div className="contenedorIcono">
         <h1 className="h1">Hola Persona</h1>
-        <img className='icono'src={Icono} alt="imagen del icono"/>
+        <NavLink to='/MiCuenta'>
+            <img className='icono'src={Icono} alt="imagen del icono"/>
+        </NavLink>
         </div>
         </div>
     );
--- paginaweb/src/pagina/Productos.js
+++ paginaweb/src/pagina/Productos.js
@@ -1,13 +1,6 @@
 import React from "react";
 import BarraNavegacion from './BarraNavegacion';
 import Encabezado from './Encabezado';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-  }from 'react-router-dom';
-
-import Cerveza from "./productos/Cervezas";
 import {NavLink} from "react-router-dom";
 import '../HojasDeEstilo/Productos.css';
 
@@ -16,13 +9,106 @@ const Productos = () => {
         <div className="contenedorProductos">
             <Encabezado/>
             <BarraNavegacion/>
-        <nav>
-        <ul className="contenedorLista">
-            <li><NavLink className="cervezas" to='/productos/cerveza'>Cervezas</NavLink></li>
-            <li><NavLink className="ron" to='/productos/ron'>Ron</NavLink></li>
-            <li><NavLink className="whisky" to='/productos/whisky'>Whisky</NavLink></li>
-        </ul>
-        </nav>
+            <nav>
+                <ul className="contenedorLista">
+                    <li><NavLink className="cervezas" to='/productos/cerveza'>Cervezas</NavLink></li>
+                    <li><NavLink className="ron" to='/productos/ron'>Ron</NavLink></li>
+                    <li><NavLink className="whisky" to='/productos/whisky'>Whisky</NavLink></li>
+                </ul>
+            </nav>
+
+            <div class="targetas">
+
+                <div class="targeta">
+                    <div class="cerveza1">
+                        <div class="imagen_cervezas1"></div>
+                        <div class="detalle">
+                            <h2>Aguila 330ml</h2>
+                            <p>Cerveza de 330ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="cerveza2">
+                        <div class="imagen_cervezas2"></div>
+                        <div class="detalle">
+                            <h2>Corona 355ml</h2>
+                            <p>Cerveza de 355ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="cerveza3">
+                        <div class="imagen_cervezas3"></div>
+                        <div class="detalle">
+                            <h2>Club Colombia 330ml</h2>
+                            <p>Cerveza de 330ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="ron1">
+                        <div class="imagen_ron1"></div>
+                        <div class="detalle">
+                            <h2>Ron Medellin 750ml</h2>
+                            <p>Ron de 750ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="ron2">
+                        <div class="imagen_ron2"></div>
+                        <div class="detalle">
+                            <h2>Viejo de Caldas 750ml</h2>
+                            <p>Ron de 750ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="ron3">
+                        <div class="imagen_ron3"></div>
+                        <div class="detalle">
+                            <h2>La Hechicera 700ml</h2>
+                            <p>Ron de 700ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="whisky1">
+                        <div class="imagen_whisky1"></div>
+                        <div class="detalle">
+                            <h2>Blue Label 750ml</h2>
+                            <p>Whisky de 750ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="whisky2">
+                        <div class="imagen_whisky2"></div>
+                        <div class="detalle">
+                            <h2>Jack Daniels 3L</h2>
+                            <p>Whisky de 3L en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="targeta">
+                    <div class="whisky3">
+                        <div class="imagen_whisky3"></div>
+                        <div class="detalle">
+                            <h2>Buchanan's 750ml</h2>
+                            <p>Whisky de 750ml en envase de vidrio.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
