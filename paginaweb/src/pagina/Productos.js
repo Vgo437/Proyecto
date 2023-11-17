f1f367df958ee3efbdd52821cf93caec42cc0da3
Cristian06Meneses <cristianmeneses979@gmail.com>
2023-11-16 23:36:22 -0500

    Arreglados los filtros de los productos

--- paginaweb/src/pagina/Productos.js
+++ paginaweb/src/pagina/Productos.js
@@ -1,14 +1,18 @@
 import React from "react";
-import BarraNavegacion from './BarraNavegacion';
-import Encabezado from './Encabezado';
-import {NavLink} from "react-router-dom";
+import Cervezas from './productos/Cervezas';
+import Ron from './productos/Ron';
+import Whisky from './productos/Whisky';
+import BarraNavegacion from "./BarraNavegacion";
+import Encabezado from "./Encabezado";
 import '../HojasDeEstilo/Productos.css';
+import {NavLink} from "react-router-dom";
 
 const Productos = () => {
     return(
         <div className="contenedorProductos">
             <Encabezado/>
             <BarraNavegacion/>
+
             <nav>
                 <ul className="contenedorLista">
                     <h3>Filtrar: </h3>
@@ -18,98 +22,9 @@ const Productos = () => {
                 </ul>
             </nav>
 
-            <div class="targetas">
-
-                <div class="targeta">
-                    <div class="cerveza1">
-                        <div class="imagen_cervezas1"></div>
-                        <div class="detalle">
-                            <h2>Aguila 330ml</h2>
-                            <p>Cerveza de 330ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="cerveza2">
-                        <div class="imagen_cervezas2"></div>
-                        <div class="detalle">
-                            <h2>Corona 355ml</h2>
-                            <p>Cerveza de 355ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="cerveza3">
-                        <div class="imagen_cervezas3"></div>
-                        <div class="detalle">
-                            <h2>Club Colombia 330ml</h2>
-                            <p>Cerveza de 330ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="ron1">
-                        <div class="imagen_ron1"></div>
-                        <div class="detalle">
-                            <h2>Ron Medellin 750ml</h2>
-                            <p>Ron de 750ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="ron2">
-                        <div class="imagen_ron2"></div>
-                        <div class="detalle">
-                            <h2>Viejo de Caldas 750ml</h2>
-                            <p>Ron de 750ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="ron3">
-                        <div class="imagen_ron3"></div>
-                        <div class="detalle">
-                            <h2>La Hechicera 700ml</h2>
-                            <p>Ron de 700ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="whisky1">
-                        <div class="imagen_whisky1"></div>
-                        <div class="detalle">
-                            <h2>Blue Label 750ml</h2>
-                            <p>Whisky de 750ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="whisky2">
-                        <div class="imagen_whisky2"></div>
-                        <div class="detalle">
-                            <h2>Jack Daniels 3L</h2>
-                            <p>Whisky de 3L en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="targeta">
-                    <div class="whisky3">
-                        <div class="imagen_whisky3"></div>
-                        <div class="detalle">
-                            <h2>Buchanan's 750ml</h2>
-                            <p>Whisky de 750ml en envase de vidrio.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Cervezas/>
+            <Ron/>
+            <Whisky/>
         </div>
     );
 }
--- paginaweb/src/pagina/productos/Cervezas.js
+++ paginaweb/src/pagina/productos/Cervezas.js
@@ -1,12 +1,47 @@
 import React from "react";
-import BarraNavegacion from "../BarraNavegacion";
-import Encabezado from "../Encabezado";
+import {NavLink} from "react-router-dom";
+
 const Cerveza = () =>{
     return (
         <div>
-        <Encabezado/>
-        <BarraNavegacion/>
-        <h1>cerveza</h1>
+            <nav>
+                <ul>
+                    <li><NavLink className="volver" to='/Productos'>Volver</NavLink></li>
+                </ul>
+            </nav>
+
+        <div class="targetas">
+            <div class="targeta">
+                <div class="cerveza1">
+                    <div class="imagen_cervezas1"></div>
+                    <div class="detalle">
+                        <h2>Aguila 330ml</h2>
+                        <p>Cerveza de 330ml en envase de vidrio.</p>
+                    </div>
+                 </div>
+            </div>
+
+            <div class="targeta">
+                    <div class="cerveza2">
+                    <div class="imagen_cervezas2"></div>
+                    <div class="detalle">
+                        <h2>Corona 355ml</h2>
+                        <p>Cerveza de 355ml en envase de vidrio.</p>
+                     </div>
+                </div>
+            </div>
+
+            <div class="targeta">
+                <div class="cerveza3">
+                    <div class="imagen_cervezas3"></div>
+                    <div class="detalle">
+                        <h2>Club Colombia 330ml</h2>
+                        <p>Cerveza de 330ml en envase de vidrio.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         </div>
     )
 }
